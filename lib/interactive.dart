5999b945d9effeaf29c11575b2503e1cc1308483
Elayabharath Elango <elayabharath@gmail.com>
2019-12-27 13:58:23 +0800

    Rearrange functions to be readable

--- lib/interactive.dart
+++ lib/interactive.dart
@@ -6,47 +6,51 @@ import 'package:avf/firebase.dart' as fb;
 
 Logger logger = Logger("interactive.dart");
 
+const DEFAULT_CHART_WIDTH = 300;
+const CHART_HEIGHT = 480;
+const CHART_PADDING = 18;
+const CHART_XAXIS_HEIGHT = 25;
+const SQ_IN_ROW = 6;
+const SQ_WIDTH = 12;
+
+const HTML_BODY_SELECTOR = "body";
 const ROW_CSS_CLASS = "row";
 const CONTAINER_CSS_CLASS = "container";
-const FILTER_COLUMN_CSS_CLASSES = ["col-lg-3", "col-md-4", "col-sm-4", "col-4"];
+
+// Filter CSS classes
+const FILTER_COLUMN_CSS_CLASSES = ["col-lg-2", "col-md-4", "col-sm-4", "col-4"];
+const FILTER_WRAPPER_CSS_CLASS = "filter-wrapper";
+const FILTER_OPTION_CSS_CLASS = "filter-option";
+
+// Chart CSS classes
 const CHART_COLUMN_CSS_CLASSES = [
   "col-lg-9",
   "col-md-9",
   "col-sm-12",
   "col-12"
 ];
+const CHART_WRAPPER_CSS_CLASS = "chart-wrapper";
+const XAXIS_CSS_CLASS = "x-axis";
+const XAXIS_LABEL_CSS_CLASS = "x-axis--label";
+
+// Messages CSS classses
 const MESSAGES_COLUMN_CSS_CLASSES = [
   "col-lg-3",
   "col-md-3",
   "col-sm-12",
   "col-12"
 ];
-const THEME_COLUMN_CSS_CLASSES = ["col-lg-3", "col-md-4", "col-sm-6", "col-6"];
-
-const DEFAULT_CHART_WIDTH = 300;
-const CHART_PADDING = 18;
-const CHART_HEIGHT = 480;
-const CHART_XAXIS_HEIGHT = 25;
-
-const HTML_BODY_SELECTOR = "body";
-const FILTER_WRAPPER_CSS_CLASS = "filter-wrapper";
-const FILTER_OPTION_CSS_CLASS = "filter-option";
-const CHART_WRAPPER_CSS_CLASS = "chart-wrapper";
 const MESSAGES_WRAPPER_CSS_CLASS = "messages-wrapper";
 const MESSAGE_CSS_CLASS = "message";
 const MESSAGES_RESPONSE_CSS_CLASS = "message-response";
 const MESSAGES_QUESTION_CSS_CLASS = "message-question";
 const PLACEHOLDER_CSS_CLASS = "placeholder";
-const XAXIS_CSS_CLASS = "x-axis";
-const XAXIS_LABEL_CSS_CLASS = "x-axis--label";
-const LEGEND_ITEM_CSS_CLASS = "legend-item";
-
 const MESSAGES_PLACEHOLDER_TEXT =
     "Click on square to view messages between the person and Africa's voices's volunteers.";
 
-// to do: make this configurable based on number of people
-const SQ_IN_ROW = 6;
-const SQ_WIDTH = 12;
+// Legend CSS classes
+const LEGEND_COLUMN_CSS_CLASSES = ["col-lg-3", "col-md-4", "col-sm-6", "col-6"];
+const LEGEND_ITEM_CSS_CLASS = "legend-item";
 
 class Interactive {
   List<model.Filter> _filters;
@@ -54,9 +58,14 @@ class Interactive {
   List<model.Message> _messages;
   List<model.Person> _people;
   num _chartWidth;
-  model.Selected _selected = model.Selected();
+  model.Selected _selected;
 
   html.DivElement _container;
+  html.DivElement _filtersWrapper;
+  html.DivElement _dataWrapper;
+  html.DivElement _chartsColumn;
+  html.DivElement _messagesColumn;
+  html.DivElement _legendWrapper;
 
   Interactive(this._container) {
     _chartWidth = _computeChartWidth();
@@ -65,9 +74,33 @@ class Interactive {
 
   init() async {
     await fb.init();
-    _loadFilters();
+    await _loadFilters();
+    await _loadThemes();
+    _selected = model.Selected();
+    _selected.updateMetric(_filters.first.value);
+    await _loadPeople();
+
+    _filtersWrapper = html.DivElement()..classes = [ROW_CSS_CLASS];
+    _dataWrapper = html.DivElement()..classes = [ROW_CSS_CLASS];
+    _chartsColumn = html.DivElement()..classes = CHART_COLUMN_CSS_CLASSES;
+    _messagesColumn = html.DivElement()..classes = MESSAGES_COLUMN_CSS_CLASSES;
+    _legendWrapper = html.DivElement()..classes = [ROW_CSS_CLASS];
+
+    _dataWrapper.append(_chartsColumn);
+    _dataWrapper.append(_messagesColumn);
+
+    _container.nodes.clear();
+    _container.append(_filtersWrapper);
+    _container.append(_dataWrapper);
+    _container.append(_legendWrapper);
+
+    _renderFilters();
+    _renderChart();
+    _renderMessages();
+    _renderLegend();
   }
 
+  // Utils
   num _computeChartWidth() {
     var width = DEFAULT_CHART_WIDTH;
     var container = html.DivElement()..classes = [CONTAINER_CSS_CLASS];
@@ -84,31 +117,23 @@ class Interactive {
     return width;
   }
 
+  String _getThemeColor(String theme) {
+    String color = "black";
+    _themes.forEach((t) {
+      if (t.value == theme) color = t.color;
+    });
+    return color;
+  }
+
+  // Data fetch
   void _loadFilters() async {
     _filters = await fb.readFilters();
+    logger.log("${_filters.length} filters loaded");
+  }
+
+  void _loadThemes() async {
     _themes = await fb.readThemes();
-    _selected.updateMetric(_filters.first.value);
-    await _loadPeople();
-    _render();
-  }
-
-  void _updateMetric(String value) async {
-    _selected.updateMetric(value);
-    await _loadPeople();
-    _render();
-  }
-
-  void _updateFilter(String value) {
-    value = value == "null" ? null : value;
-    _selected.updateFilter(value);
-    _render();
-  }
-
-  void _updateFilterOption(String value) async {
-    value = value == "null" ? null : value;
-    _selected.updateOption(value);
-    await _loadPeople();
-    _render();
+    logger.log("${_themes.length} themes loaded");
   }
 
   void _loadPeople() async {
@@ -116,62 +141,98 @@ class Interactive {
     logger.log("${_people.length} people loaded");
   }
 
-  void _loadMessages(String id) async {
-    _messages = await fb.readMessages(id);
+  void _loadMessages(String personID) async {
+    _messages = await fb.readMessages(personID);
     logger.log("${_messages.length} messages loaded");
+  }
+
+  // User events
+  void _updateMetric(String value) async {
+    _selected.updateMetric(value);
+    await _loadPeople();
+    _renderFilters();
+    _renderChart();
+    _messages = null;
     _renderMessages();
   }
 
-  html.DivElement _renderMetricDropdown() {
-    var metricWrapper = html.DivElement()..classes = FILTER_COLUMN_CSS_CLASSES;
-    var metricLabel = html.LabelElement()..innerText = "View by";
-    var metricSelect = html.SelectElement()
+  void _updateFilter(String value) {
+    value = value == "null" ? null : value;
+    _selected.updateFilter(value);
+    _renderFilters();
+  }
+
+  void _updateFilterOption(String value) async {
+    value = value == "null" ? null : value;
+    _selected.updateOption(value);
+    await _loadPeople();
+    _renderFilters();
+    _renderChart();
+    _messages = null;
+    _renderMessages();
+  }
+
+  void handleMouseEnter(svg.SvgElement rect, int sqWidth) {
+    rect.parent.append(rect);
+    rect
+      ..setAttribute(
+          "transform", "translate(${-2 * sqWidth}, ${-2 * sqWidth}) scale(2)");
+  }
+
+  void handleMouseOut(svg.SvgElement rect) {
+    rect..setAttribute("transform", "translate(0, 0) scale(1)");
+  }
+
+  // Render
+  html.DivElement _getMetricDropdown() {
+    var wrapper = html.DivElement()..classes = FILTER_COLUMN_CSS_CLASSES;
+    var label = html.LabelElement()..innerText = "View by";
+    var select = html.SelectElement()
       ..onChange
           .listen((e) => _updateMetric((e.target as html.SelectElement).value));
     _filters.forEach((filter) {
-      var metricOption = html.OptionElement()
+      var option = html.OptionElement()
         ..value = filter.value
         ..innerText = filter.label
         ..selected = filter.value == _selected.metric;
-      metricSelect.append(metricOption);
+      select.append(option);
     });
 
-    metricWrapper.append(metricLabel);
-    metricWrapper.append(metricSelect);
-    return metricWrapper;
+    wrapper.append(label);
+    wrapper.append(select);
+    return wrapper;
   }
 
-  html.DivElement _renderFilterDropdown() {
-    var filterWrapper = html.DivElement()..classes = FILTER_COLUMN_CSS_CLASSES;
-    var filterLabel = html.LabelElement()..innerText = "Filter by";
-    var filterSelect = html.SelectElement()
+  html.DivElement _getFilterDropdown() {
+    var wrapper = html.DivElement()..classes = FILTER_COLUMN_CSS_CLASSES;
+    var label = html.LabelElement()..innerText = "Filter by";
+    var select = html.SelectElement()
       ..onChange
           .listen((e) => _updateFilter((e.target as html.SelectElement).value));
     var emptyOption = html.OptionElement()
       ..innerText = "--"
       ..value = null
       ..selected = _selected.filter == null;
-    filterSelect.append(emptyOption);
+    select.append(emptyOption);
     _filters.forEach((filter) {
-      var filterOption = html.OptionElement()
+      var option = html.OptionElement()
         ..value = filter.value
         ..innerText = filter.label
         ..selected = filter.value == _selected.filter;
       if (filter.value != _selected.metric) {
-        filterSelect.append(filterOption);
+        select.append(option);
       }
     });
 
-    filterWrapper.append(filterLabel);
-    filterWrapper.append(filterSelect);
-    return filterWrapper;
+    wrapper.append(label);
+    wrapper.append(select);
+    return wrapper;
   }
 
-  html.DivElement _renderFilterOptionDropdown() {
-    var filterOptionWrapper = html.DivElement()
-      ..classes = FILTER_COLUMN_CSS_CLASSES;
-    var filterOptionLabel = html.LabelElement()..innerText = ".";
-    var filterOptionSelect = html.SelectElement()
+  html.DivElement _getFilterOptionDropdown() {
+    var wrapper = html.DivElement()..classes = FILTER_COLUMN_CSS_CLASSES;
+    var label = html.LabelElement()..innerText = ".";
+    var select = html.SelectElement()
       ..classes = [FILTER_OPTION_CSS_CLASS]
       ..onChange.listen(
           (e) => _updateFilterOption((e.target as html.SelectElement).value));
@@ -179,25 +240,42 @@ class Interactive {
       ..innerText = "--"
       ..value = null
       ..selected = _selected.option == null;
-    filterOptionSelect.append(emptyOption);
+    select.append(emptyOption);
 
     var currentFilter =
         _filters.firstWhere((filter) => filter.value == _selected.filter);
     currentFilter.options.forEach((filter) {
-      var filterOption = html.OptionElement()
+      var option = html.OptionElement()
         ..value = filter.value
         ..innerText = filter.label
         ..selected = filter.value == _selected.option;
-      filterOptionSelect.append(filterOption);
+      select.append(option);
     });
 
-    filterOptionWrapper.append(filterOptionLabel);
-    filterOptionWrapper.append(filterOptionSelect);
-    return filterOptionWrapper;
+    wrapper.append(label);
+    wrapper.append(select);
+    return wrapper;
   }
 
-  html.DivElement _renderChart() {
-    var chartWrapper = html.DivElement()..classes = [CHART_WRAPPER_CSS_CLASS];
+  void _renderFilters() {
+    _filtersWrapper.nodes.clear();
+
+    _filtersWrapper.append(_getMetricDropdown());
+    _filtersWrapper.append(_getFilterDropdown());
+    if (_selected.filter != null) {
+      _filtersWrapper.append(_getFilterOptionDropdown());
+    }
+  }
+
+  void _loadRenderMessages(String personID) async {
+    await _loadMessages(personID);
+    _renderMessages();
+  }
+
+  void _renderChart() {
+    _chartsColumn.nodes.clear();
+
+    var wrapper = html.DivElement()..classes = [CHART_WRAPPER_CSS_CLASS];
     var chartWidth = _chartWidth - (4 * CHART_PADDING);
 
     var svgContainer = svg.SvgSvgElement()
@@ -269,7 +347,7 @@ class Interactive {
 
         var sqGroup = svg.SvgElement.tag("g")
           ..setAttribute("transform-origin", "$xOrigin $yOrigin")
-          ..onClick.listen((e) => this._loadMessages(colData[j].id))
+          ..onClick.listen((e) => _loadRenderMessages(colData[j].id))
           ..onMouseEnter
               .listen((e) => this.handleMouseEnter(e.currentTarget, SQ_WIDTH))
           ..onMouseOut.listen((e) => this.handleMouseOut(e.currentTarget));
@@ -303,57 +381,23 @@ class Interactive {
       svgContainer.append(colSVG);
     }
 
-    return chartWrapper..append(svgContainer);
+    wrapper..append(svgContainer);
+    _chartsColumn.append(wrapper);
   }
 
-  void handleMouseEnter(svg.SvgElement rect, int sqWidth) {
-    rect.parent.append(rect);
-    rect
-      ..setAttribute(
-          "transform", "translate(${-2 * sqWidth}, ${-2 * sqWidth}) scale(2)");
-  }
+  void _renderMessages() {
+    _messagesColumn.nodes.clear();
+    var wrapper = html.DivElement()..classes = [MESSAGES_WRAPPER_CSS_CLASS];
 
-  void handleMouseOut(svg.SvgElement rect) {
-    rect..setAttribute("transform", "translate(0, 0) scale(1)");
-  }
-
-  String _getThemeColor(String theme) {
-    String color = "black";
-    _themes.forEach((t) {
-      if (t.value == theme) color = t.color;
-    });
-    return color;
-  }
-
-  html.DivElement _renderLegend() {
-    var legendWrapper = html.DivElement()..classes = [ROW_CSS_CLASS];
-    _themes.forEach((theme) {
-      var legendColumn = html.DivElement()..classes = THEME_COLUMN_CSS_CLASSES;
-      var legendColor = html.LabelElement()
-        ..classes = [LEGEND_ITEM_CSS_CLASS]
-        ..innerText = theme.label
-        ..style.borderLeftColor = theme.color;
-      legendColumn.append(legendColor);
-
-      legendWrapper.append(legendColumn);
-    });
-
-    return legendWrapper;
-  }
-
-  html.DivElement _renderMessages() {
     if (_messages == null) {
-      var messagesWrapper = html.DivElement()
-        ..classes = [MESSAGES_WRAPPER_CSS_CLASS];
       var placeholderText = html.ParagraphElement()
         ..classes = [PLACEHOLDER_CSS_CLASS]
         ..appendText(MESSAGES_PLACEHOLDER_TEXT);
-      messagesWrapper.append(placeholderText);
-      return messagesWrapper;
+      wrapper.append(placeholderText);
+      _messagesColumn.append(wrapper);
+      return;
     }
 
-    var wrapper = html.querySelector("#$MESSAGES_WRAPPER_CSS_CLASS");
-    wrapper.classes.toggle(MESSAGES_WRAPPER_CSS_CLASS, true);
     wrapper.style.height = (CHART_HEIGHT + 2 * CHART_PADDING).toString() + "px";
     wrapper.children.clear();
 
@@ -372,30 +416,21 @@ class Interactive {
             : "white";
       wrapper.append(messageDiv);
     }
-    return wrapper;
+
+    _messagesColumn.append(wrapper);
   }
 
-  void _render() {
-    var filtersWrapper = html.DivElement()
-      ..classes = [ROW_CSS_CLASS, FILTER_WRAPPER_CSS_CLASS];
-    filtersWrapper.append(_renderMetricDropdown());
-    filtersWrapper.append(_renderFilterDropdown());
-    if (_selected.filter != null) {
-      filtersWrapper.append(_renderFilterOptionDropdown());
-    }
+  void _renderLegend() {
+    _legendWrapper.nodes.clear();
+    _themes.forEach((theme) {
+      var legendColumn = html.DivElement()..classes = LEGEND_COLUMN_CSS_CLASSES;
+      var legendColor = html.LabelElement()
+        ..classes = [LEGEND_ITEM_CSS_CLASS]
+        ..innerText = theme.label
+        ..style.borderLeftColor = theme.color;
+      legendColumn.append(legendColor);
 
-    var chartMessageRow = html.DivElement()..classes = [ROW_CSS_CLASS];
-    var chartColumn = html.DivElement()..classes = CHART_COLUMN_CSS_CLASSES;
-    var messageColumn = html.DivElement()
-      ..classes = MESSAGES_COLUMN_CSS_CLASSES;
-    chartMessageRow.append(chartColumn);
-    chartColumn.append(_renderChart());
-    chartMessageRow.append(messageColumn);
-    messageColumn.append(_renderMessages());
-
-    _container.children.clear();
-    _container.append(filtersWrapper);
-    _container.append(chartMessageRow);
-    _container.append(_renderLegend());
+      _legendWrapper.append(legendColumn);
+    });
   }
 }

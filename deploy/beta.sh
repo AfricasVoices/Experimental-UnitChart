e16e9343dcddaf0f150b0b80ad9b163b6539e1b3
Elayabharath Elango <elayabharath@gmail.com>
2020-01-11 14:39:50 +0530

    Beta deploy for firebase

---
+++ deploy/beta.sh
@@ -0,0 +1,9 @@
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+PATH_TO_CONSTANTS_FILE="deploy/fb_beta_constants.json";
+
+cp $PATH_TO_CONSTANTS_FILE web/assets/firebase_constants.json
+
+# Dev command
+webdev serve --auto=refresh
